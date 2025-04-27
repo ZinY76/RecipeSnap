1975e8b94362034de7a1ac936f3b0cf69b27f4f5
ZinY <ZinY.SG@gmail.com>
2025-04-27 16:26:06 +0000

    Changed the logo from picsum.photos to Facebook

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: 'www.facebook.com',
         port: '',
         pathname: '/**',
       },
